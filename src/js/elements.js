fdbe1ce580690414446c420863a8740029eb9393
Amr Hassan <63314726+amrhassan93@users.noreply.github.com>
2020-09-05 21:55:24 +0200

    reverting

--- src/js/elements.js
+++
@@ -1,4 +0,0 @@
-export let searchBtn = document.getElementById('searchBtn');
-export let searchInput = document.getElementById('searchInput');
-export let searchResultContainer = document.getElementById('searchResult');
-export let recipeDetailsContainer = document.getElementById('recipeDetails');
